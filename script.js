186b36f3a8cb88e5c209a9d58dc2b77c868328cf
LOKESH KUMAR <lokeshyadav05299@gmail.com>
2026-01-21 20:10:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -141,5 +141,16 @@ document.querySelectorAll(".tilt").forEach(card => {
   });
 });
 
+const slides = document.querySelectorAll(".slide");
+let index = 0;
+
+if (slides.length > 0) {
+  setInterval(() => {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+  }, 5000);
+}
+
 
 
