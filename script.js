40b90801d2e0ddf7e27bc05bb1bf7ae97379c9af
LOKESH KUMAR <lokeshyadav05299@gmail.com>
2026-01-22 16:06:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -140,6 +140,15 @@ document.querySelectorAll(".tilt").forEach(card => {
     card.style.transform = "rotateX(0) rotateY(0)";
   });
 });
+const slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}, 5000);
+
 
 
 
